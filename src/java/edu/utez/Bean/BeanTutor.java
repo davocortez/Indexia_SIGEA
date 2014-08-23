fc9ed18171453fa5c1f223a03d68ba96e3d6775a
davocortez <david_cortez6@hotmail.com>
2014-08-23 00:51:00 -0500

    Se agrego BeanTutor

---
+++ src/java/edu/utez/Bean/BeanTutor.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.utez.Bean;
+
+/**
+ *
+ * @author David
+ */
+public class BeanTutor {
+    public String nombre;
+    public String cuatrimestre;
+    public String grupo;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCuatrimestre() {
+        return cuatrimestre;
+    }
+
+    public void setCuatrimestre(String cuatrimestre) {
+        this.cuatrimestre = cuatrimestre;
+    }
+
+    public String getGrupo() {
+        return grupo;
+    }
+
+    public void setGrupo(String grupo) {
+        this.grupo = grupo;
+    }
+    
+}
